82b49bb8e06dd2fb1526686309bea4e1f1bfcb2d
Shelby Stanton <shelby_@hotmail.co.uk>
2017-09-20 22:07:23 +0100

    Adding styles, images and content.

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,9 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <link rel="stylesheet" href="/css/main.css">
     </head>
     <body>
-        @yield('body')
+
+    	@include('_partials.header')
+
+    	<main>
+
+	        @yield('body')
+	        
+	    </main>
     </body>
 </html>
